f2fb0c5161a57442da0f163fb054f0152a939ea0
vy vo <tuonggvy.vo@gmail.com>
2019-11-06 04:02:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+  first_names = []
+  i = 0
+  while 
+  first_names << name.split(" ").first
+end
 
-
+my_collect(name) { |name| name.split(" ").first}
